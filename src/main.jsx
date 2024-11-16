0b302f53aea86662880be8b4a95033a6a613c67c
Souparno <souparna2000shee@gmail.com>
2024-11-16 18:20:54 +0530

    change 2

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.scss'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { Store } from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
 
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={Store}>
       <StrictMode>
         <App />
       </StrictMode>,
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 )
